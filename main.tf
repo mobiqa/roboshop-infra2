13959ea3be60f33ee13f2b70e42072132d596bd2
venkat <venkat.mobiqa@gmail.com>
2023-05-11 09:19:21 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
 #
   for_each = var.vpc
   cidr_block = each.value.cidr_block
-#  public_subnets_cidr  = each.value.public_subnets_cidr
+  subnets_cidr  = each.value.public_subnets_cidr
 #  private_subnets_cidr = each.value.private_subnets_cidr
 #  availability_zones = each.value.availability_zones
 
