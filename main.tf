1d0938e2b0f0e943cbf3363a0c681e21133a9cfb
venkat <venkat.mobiqa@gmail.com>
2023-05-12 08:37:32 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -2,10 +2,26 @@ module "vpc" {
   source         = "github.com/mobiqa/tf-module-vpc2"
   env            =  var.env
   default_vpc_id = var.default_vpc_id
-#
+
+
   for_each = var.vpc
   cidr_block = each.value.cidr_block
-  
+
+}
+
+
+module "subnets" {
+  source         = "github.com/mobiqa/tf-module-subnets2"
+  env            =  var.env
+  default_vpc_id = var.default_vpc_id
+  vpc_id         = module.vpc.vpc_id
+
+  for_each = var.subnets
+  cidr_block = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name              = each.value.name
+
+
 }
 
 
