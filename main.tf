280a10d31c8c44367b523eae5dd5bbe2388f7473
venkat <venkat.mobiqa@gmail.com>
2023-05-12 17:36:48 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -6,25 +6,28 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value.cidr_block
+  public_subnets    = each.value.public_subnets
+  private_subnets   = each.value.private_subnets
+  availability_zone = each.value.availability_zone
 
 }
 
-
-module "subnets" {
-  source         = "github.com/mobiqa/tf-module-subnets2"
-  env            =  var.env
-  default_vpc_id = var.default_vpc_id
-
-
-  for_each = var.subnets
-  cidr_block = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  name              = each.value.name
-  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null)
-  vpc_peering_connection_id   = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_peering_connection_id", null)
-#  internet_gw_id = lookup(lookup(module.vpc, each.value.vpc_name, null ), "internet_gw_id", null)
-  internet_gw         = lookup(each.value, "internet_gw", false )
-  nat_gw         = lookup(each.value, "nat_gw", false )
+#
+#module "subnets" {
+#  source         = "github.com/mobiqa/tf-module-subnets2"
+#  env            =  var.env
+#  default_vpc_id = var.default_vpc_id
+#
+#
+#  for_each = var.subnets
+#  cidr_block = each.value.cidr_block
+#  availability_zone = each.value.availability_zone
+#  name              = each.value.name
+#  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null)
+#  vpc_peering_connection_id   = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_peering_connection_id", null)
+##  internet_gw_id = lookup(lookup(module.vpc, each.value.vpc_name, null ), "internet_gw_id", null)
+#  internet_gw         = lookup(each.value, "internet_gw", false )
+#  nat_gw         = lookup(each.value, "nat_gw", false )
 
 
 }
