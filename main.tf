4e7e351901fee5c28dcfc59b17c73057d8709bbd
venkat <venkat.mobiqa@gmail.com>
2023-05-16 07:48:36 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ module "apps" {
   source = "github.com/mobiqa/tf-module-app2"
   env    = var.env
 
-  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
+#  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
 
   for_each          = var.apps
   subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
