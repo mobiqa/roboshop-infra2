e7430086cda657a21292f19921768cd9c892a3cc
venkat <venkat.mobiqa@gmail.com>
2023-05-28 09:10:06 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -100,7 +100,33 @@ module "apps" {
   monitor_cidr = var.monitor_cidr
 
 }
+// Load Test Machine
+resource "aws_spot_instance_request" "load" {
+  instance_type          = "t3.medium"
+  ami                    = "ami-0b5a2b5b8f2be4ec2"
+  subnet_id              = "subnet-06b4a5240a6690f30"
+  vpc_security_group_ids = ["sg-0b76619b671c82bea"]
+  wait_for_fulfillment   = true
+}
 
-output "alb" {
-  value = module.alb
+resource "aws_ec2_tag" "tag" {
+  resource_id = aws_spot_instance_request.load.spot_instance_id
+  key         = "Name"
+  value       = "load-runner"
+}
+
+resource "null_resource" "apply" {
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_spot_instance_request.load.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+    inline = [
+      "curl -s -L https://get.docker.com | bash",
+      "systemctl enable docker",
+      "systemctl start docker",
+      "docker pull robotshop/rs-load"
+    ]
+  }
 }
