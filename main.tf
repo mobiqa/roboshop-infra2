535cf48c5f93e599c0fa4aae4d7b40eac7a97b6d
venkat <venkat.mobiqa@gmail.com>
2023-06-16 07:26:33 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -75,31 +75,31 @@ module "alb" {
   subnets_name = each.value.subnets_name
   internal     = each.value.internal
 }
-module "apps" {
-  source = "github.com/mobiqa/tf-module-app2"
-  env    = var.env
-
-  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
-
-  for_each          = var.apps
-  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
-  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
-  alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
-  listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
-  alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
-  component         = each.value.component
-  app_port          = each.value.app_port
-  max_size          = each.value.max_size
-  min_size          = each.value.min_size
-  desired_capacity  = each.value.desired_capacity
-  instance_type     = each.value.instance_type
-  listener_priority = each.value.listener_priority
-
-  bastion_cidr = var.bastion_cidr
-  monitor_cidr = var.monitor_cidr
-
-}
+#module "apps" {
+#  source = "github.com/mobiqa/tf-module-app2"
+#  env    = var.env
+#
+#  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
+#
+#  for_each          = var.apps
+#  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
+#  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
+#  alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
+#  listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
+#  alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
+#  component         = each.value.component
+#  app_port          = each.value.app_port
+#  max_size          = each.value.max_size
+#  min_size          = each.value.min_size
+#  desired_capacity  = each.value.desired_capacity
+#  instance_type     = each.value.instance_type
+#  listener_priority = each.value.listener_priority
+#
+#  bastion_cidr = var.bastion_cidr
+#  monitor_cidr = var.monitor_cidr
+#
+#}
 // Load Test Machine
 #resource "aws_spot_instance_request" "load" {
 #  instance_type          = "t3.medium"
@@ -133,34 +133,34 @@ module "apps" {
 
 
 
-#module "minikube" {
-#  source = "github.com/scholzj/terraform-aws-minikube"
-#
-#  aws_region        = "us-east-1"
-#  cluster_name      = "minikube"
-#  aws_instance_type = "t3.medium"
-#  ssh_public_key    = "~/.ssh/id_rsa.pub"
-#  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "public", null), "subnet_ids", null), 0)
-#  //ami_image_id        = data.aws_ami.ami.id
-#  hosted_zone         = var.hosted_zone
-#  hosted_zone_private = false
-#
-#  tags = {
-#    Application = "Minikube"
-#  }
-#
-#  addons = [
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-#  ]
-#}
-#
-#output "MINIKUBE_SERVER" {
-#  value = "ssh centos@${module.minikube.public_ip}"
-#}
-#
-#output "KUBE_CONFIG" {
-#  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
-#}
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region        = "us-east-1"
+  cluster_name      = "minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key    = "~/.ssh/id_rsa.pub"
+  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "public", null), "subnet_ids", null), 0)
+  //ami_image_id        = data.aws_ami.ami.id
+  hosted_zone         = var.hosted_zone
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
+
+output "MINIKUBE_SERVER" {
+  value = "ssh centos@${module.minikube.public_ip}"
+}
+
+output "KUBE_CONFIG" {
+  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+}
