00292dca4aa004d0db2b97c3d6838f97e795d002
venkat <venkat.mobiqa@gmail.com>
2023-06-27 11:20:56 +0530

    updating ./roboshop-infra2

--- main.tf
+++ main.tf
@@ -75,31 +75,31 @@ module "alb" {
   subnets_name = each.value.subnets_name
   internal     = each.value.internal
 }
-module "apps" {
-  source = "github.com/mobiqa/tf-module-app2"
-  env    = var.env
-
-  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
-
-  for_each          = var.apps
-  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
-  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
-  alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
-  listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
-  alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
-  component         = each.value.component
-  app_port          = each.value.app_port
-  max_size          = each.value.max_size
-  min_size          = each.value.min_size
-  desired_capacity  = each.value.desired_capacity
-  instance_type     = each.value.instance_type
-  listener_priority = each.value.listener_priority
-
-  bastion_cidr = var.bastion_cidr
-  monitor_cidr = var.monitor_cidr
-
-}
+#module "apps" {
+#  source = "github.com/mobiqa/tf-module-app2"
+#  env    = var.env
+#
+#  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
+#
+#  for_each          = var.apps
+#  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
+#  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
+#  alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
+#  listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
+#  alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
+#  component         = each.value.component
+#  app_port          = each.value.app_port
+#  max_size          = each.value.max_size
+#  min_size          = each.value.min_size
+#  desired_capacity  = each.value.desired_capacity
+#  instance_type     = each.value.instance_type
+#  listener_priority = each.value.listener_priority
+#
+#  bastion_cidr = var.bastion_cidr
+#  monitor_cidr = var.monitor_cidr
+#
+#}
 // Load Test Machine
 #resource "aws_spot_instance_request" "load" {
 #  instance_type          = "t3.medium"
